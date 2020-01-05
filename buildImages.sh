a86bbfd0c6f6211a3237c75de4e166bc887aff7b
Yves Schumann <y.schumann@yetnet.ch>
2020-01-05 21:16:10 +0100

    Implemented switch for ARM builds

--- buildImages.sh
+++ buildImages.sh
@@ -20,15 +20,18 @@ helpMe() {
     Usage:
     ${0} [options]
     Optional parameters:
+    -a  Also build ARM images beside AMD64
     -p  Publish image on DockerHub
     -h  Show this help
     "
 }
 
 PUBLISH_IMAGE=false
+BUILD_ARM_IMAGES=false
 
-while getopts ph? option; do
+while getopts aph? option; do
     case ${option} in
+        a) BUILD_ARM_IMAGES=true;;
         p) PUBLISH_IMAGE=true;;
         h|?) helpMe && exit 0;;
         *) die 90 "invalid option \"${OPTARG}\"";;
@@ -45,14 +48,16 @@ if ${PUBLISH_IMAGE} ; then
     docker push starwarsfan/edomi-baseimage:amd64-latest
 fi
 
-docker build -f arm32v7.Builder.Dockerfile -t starwarsfan/edomi-baseimage-builder:arm32v7-latest .
-if ${PUBLISH_IMAGE} ; then
-    echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
-#    docker push starwarsfan/edomi-baseimage-builder:arm32v7-latest
-fi
+if ${BUILD_ARM_IMAGES} ; then
+    docker build -f arm32v7.Builder.Dockerfile -t starwarsfan/edomi-baseimage-builder:arm32v7-latest .
+    if ${PUBLISH_IMAGE} ; then
+        echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
+    #    docker push starwarsfan/edomi-baseimage-builder:arm32v7-latest
+    fi
 
-docker build -f arm32v7.Dockerfile -t starwarsfan/edomi-baseimage:arm32v7-latest .
-if ${PUBLISH_IMAGE} ; then
-    echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
-#    docker push starwarsfan/edomi-baseimage:arm32v7-latest
+    docker build -f arm32v7.Dockerfile -t starwarsfan/edomi-baseimage:arm32v7-latest .
+    if ${PUBLISH_IMAGE} ; then
+        echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
+    #    docker push starwarsfan/edomi-baseimage:arm32v7-latest
+    fi
 fi
